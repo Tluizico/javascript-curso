13cab1050d30932b3635e1f04ffca64a6d6f03b9
Tluizico <thiagoluiz.business@gmail.com>
2022-04-09 11:59:09 -0300

    meu primeiro commit

---
+++ A5 Desafio Sentenca e Bloco .js
@@ -0,0 +1,40 @@
+/*
+Desafio Sentenca e bloco: 
+Colocar os numeros Impares até o 10 em um bloco e os numeros pares ate 10 em outro bloco.
+
+//Bloco 1
+1
+3
+5
+7
+9
+
+//Bloco 2
+2
+4
+6
+8
+10
+*/
+
+
+{
+    console.log ('Impar 1')
+    console.log ('Impar 3')
+    console.log ('Impar 5')
+    console.log ('Impar 7')
+    console.log ('Impar 9')
+    
+}
+
+{
+    console.log ('Par 2')
+    console.log ('Par 4')
+    console.log ('Par 6')
+    console.log ('Par 8')
+    console.log ('Par 10')
+
+}
+
+
+
